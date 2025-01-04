be7e74f51ceea43c64f0ee11541cde49e7b621db
Artcs <ju.jeffri.v@gmail.com>
2025-01-04 15:10:45 -0500

    Update problema1.py

--- problema1.py
+++ problema1.py
@@ -1,4 +1,21 @@
+def dp(weights, values, W):
 
+	m = len(weights)
+	memo = [float('inf')] * (100000 + 1)
+	memo[0] = 0
+
+	for i in range(m):
+		weight = weights[i]
+		value  = values[i]
+
+		for w in range(weight, 100001):
+			memo[w] = min(memo[w], memo[w-weight] + value)
+
+	res = float('inf')
+	for w in range(W, 100001):
+		res = min(res, memo[w])
+
+	return res
 
 def main():
 
@@ -6,8 +23,12 @@ def main():
 	H = list(map(int, input().split()))
 	D = list(map(int, input().split()))
 
-	
+	total = 0
+	for d in D:
+		total+=d
+
+	r = dp(H,D,n)
+	print(total-r)
+
 if __name__ == '__main__':
 	main()
-
-
